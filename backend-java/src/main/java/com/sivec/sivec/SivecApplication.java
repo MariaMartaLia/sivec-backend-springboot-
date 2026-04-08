0d9ee7c9533eddf466855da9ce0987165fd87d16
Maria Marta Almeida Brito <liacode19@gmail.com>
2026-04-08 15:41:28 -0300

    feat: add backend-java whith Spring Boot, PostgreSQL and Docker  Setup.

---
+++ backend-java/src/main/java/com/sivec/sivec/SivecApplication.java
@@ -0,0 +1,13 @@
+package com.sivec.sivec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SivecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SivecApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/sivec/sivec/SivecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivec.sivec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SivecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
